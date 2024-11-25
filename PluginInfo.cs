f5fb91aa1645187822cdf17b104f331f4ebd304d
CO <numpad0000@gmail.com>
2024-11-25 20:58:14 +0000

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace AmIPlayingGTAG
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.breathemonoxide.gorillatag.amiplayinggtag";
+        public const string Name = "AmIPlayingGTAG";
+        public const string Version = "1.0.0";
+    }
+}
